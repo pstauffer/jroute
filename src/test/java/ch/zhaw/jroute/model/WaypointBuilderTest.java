18e5ff742bb0a512422db0568ca120f0ba5e4fe1
barneyyy844 <github@nerdherd.ch>
2013-05-21 22:38:55 +0200

    bugfixing

--- src/test/java/ch/zhaw/jroute/model/WaypointBuilderTest.java
+++ src/test/java/ch/zhaw/jroute/model/WaypointBuilderTest.java
@@ -1,11 +1,8 @@
 package ch.zhaw.jroute.model;
 
-import static org.junit.Assert.*;
-
 import gov.nasa.worldwind.geom.Angle;
 import gov.nasa.worldwind.geom.Position;
 
-import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -13,7 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class WaypointBuilderTest {
-	
+
 	private IWaypointBuilder builder;
 	private WaypointTestObserver observer;
 	private double testLat = 55.55;
@@ -23,65 +20,66 @@ public class WaypointBuilderTest {
 	public void setUp() throws Exception {
 		builder = new WaypointBuilder();
 		observer = new WaypointTestObserver();
-		
+
 		builder.registerObserver(observer);
 	}
 
 	@Test
 	public void createWaypointFromPositionTest() {
-		
+
 		Angle lat = Angle.fromDegreesLatitude(testLat);
 		Angle lon = Angle.fromDegreesLatitude(testLon);
-		
-		builder.createWaypointFromPosition(new Position(lat,lon,0));
-		
-		assert(observer.isNotified());
-		
-		assert(!builder.getWaypointList().isEmpty());
-		
-		assert(observer.getNotifierObj().getLat() == testLat);
-		assert(observer.getNotifierObj().getLon() == testLon);
-		
+
+		builder.createWaypointFromPosition(new Position(lat, lon, 0));
+
+		assert (observer.isNotified());
+
+		assert (!builder.getWaypointList().isEmpty());
+
+		assert (observer.getNotifierObj().getLat() == testLat);
+		assert (observer.getNotifierObj().getLon() == testLon);
+
 		observer.reset();
+		System.out.println("blaa");
 	}
-	
+
 	@Test
 	public void setStartWaypointTest() {
 		builder.setStartWaypoint(WaypointStatusEnum.start, new Waypoint());
-		
-		assert(observer.isStart());
-		
-		assert(builder.getStartWaypoint()!=null);
-		
+
+		assert (observer.isStart());
+
+		assert (builder.getStartWaypoint() != null);
+
 		observer.reset();
 	}
-	
+
 	@Test
-	public void setEndWaypoin() {
+	public void setEndWaypoint() {
 		builder.setEndWaypoint(WaypointStatusEnum.end, new Waypoint());
-		
-		assert(observer.isEnd());
-		
-		assert(builder.getEndWaypoint()==null);
-		
+
+		assert (observer.isEnd());
+
+		assert (builder.getEndWaypoint() == null);
+
 		observer.reset();
 	}
-	
-	private class WaypointTestObserver implements Observer{
+
+	private class WaypointTestObserver implements Observer {
 
 		private boolean notified = false;
 		private boolean start = false;
 		private boolean end = false;
-	
+
 		private Waypoint notifierObj;
-		
+
 		@Override
 		public void update(Observable arg0, Object arg1) {
 			notified = true;
-			
-			notifierObj = (Waypoint)arg1;
-			
-			switch(notifierObj.getStatus()){
+
+			notifierObj = (Waypoint) arg1;
+
+			switch (notifierObj.getStatus()) {
 			case undefined:
 				break;
 			case start:
@@ -93,7 +91,7 @@ public class WaypointBuilderTest {
 			default:
 				break;
 			}
-			
+
 		}
 
 		public boolean isNotified() {
@@ -106,7 +104,7 @@ public class WaypointBuilderTest {
 			this.end = false;
 			notifierObj = null;
 		}
-		
+
 		public boolean isStart() {
 			return start;
 		}
@@ -114,15 +112,11 @@ public class WaypointBuilderTest {
 		public boolean isEnd() {
 			return end;
 		}
-		
+
 		public Waypoint getNotifierObj() {
 			return notifierObj;
 		}
 
-		
-		
 	}
 
 }
-
-
